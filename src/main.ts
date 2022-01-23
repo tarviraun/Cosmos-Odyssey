c4872417bbf6546b5da381a7a64adbaa1fe433bd
Tarvi Raun <tarvi.raun@twn.ee>
2022-01-23 19:57:53 +0200

    Added swagger builder with main setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ErrorsFilter } from './shared/filters/error.filter';
+
+const PORT = process.env.PORT || 3000;
+const PREFIX = 'cosmos-odyssey-backend';
+const DATABASE_URL = process.env.MONGODB_CONNECTION_STRING;
+const apiName = 'Cosmos odysssey backend API';
+const corsOrigin = process.env.CORS_ORIGIN;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appOptions = {
+    cors: { origin: corsOrigin },
+    logging: true,
+  };
+  const app = await NestFactory.create(AppModule, appOptions);
+
+  app.useGlobalFilters(new ErrorsFilter());
+  app.setGlobalPrefix(`/${PREFIX}`);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Cosmos odyssey backend')
+    .setDescription(apiName)
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup(`${PREFIX}/api`, app, document);
+
+  try {
+    await app.listen(PORT);
+
+    Logger.log(`${apiName} starter successfully and running on port ${PORT}`);
+    Logger.log(`${apiName} is connected to ${DATABASE_URL} database`);
+  } catch (error) {
+    Logger.error(`Could not start ${apiName}`, error);
+  }
 }
 bootstrap();
