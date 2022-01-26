06c521f2a2601a43952e52099ce82382c19d1e1c
Tarvi Raun <tarvi.raun@twn.ee>
2022-01-26 21:57:11 +0200

    Added databse connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PricelistModule } from './pricelist/pricelist.module';
 
 @Module({
-  imports: [PricelistModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.prelive', '.env'],
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING, {
+      dbName: 'cosmos-odyssey',
+      useNewUrlParser: true,
+    }),
+    PricelistModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/pricelist/pricelist.module.ts
+++ src/pricelist/pricelist.module.ts
@@ -1,11 +1,18 @@
 import { HttpModule, Module } from '@nestjs/common';
-import { TralvelPricesApiService } from 'src/shared/services/travel-prices-api/travel-prices-api.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TravelPricesApiService } from 'src/shared/services/travel-prices-api/travel-prices-api.service';
 import { PricelistController } from './controllers/pricelist.controller';
+import { Pricelist, PricelistSchema } from './schemas/pricelist.schema';
 import { PricelistService } from './services/pricelist.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Pricelist.name, schema: PricelistSchema },
+    ]),
+    HttpModule,
+  ],
   controllers: [PricelistController],
-  providers: [PricelistService, TralvelPricesApiService],
+  providers: [PricelistService, TravelPricesApiService],
 })
 export class PricelistModule {}
